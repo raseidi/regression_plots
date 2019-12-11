09e73618aae9bdb5f3aa69ca7fde0fbe71f0bb2f
seidi <rso.seidi@gmail.com>
2019-12-11 19:06:42 -0300

    first commit

---
+++ src/feature_importance.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from my_plots import single_plot
+
+def format_feat_imp(scores, feature_names, save_path=None):
+    '''
+        scores: list of feature importances (if scores are retrieved
+        from cross validation, pass scores like:
+            [fi.feature_importances_ for fi in scores['estimator']]
+        )
+        feature_names: columns of original dataframe
+        save_path: None is you do not wish to save it, the path otherwise
+        returns feature importances (mean for cv) as dataframe
+    '''
+    scores = np.array(scores)
+    df = pd.DataFrame(scores if scores.ndim == 1 else scores[0], index=feature_names, columns=['Importance'])
+    if scores.ndim > 1:
+        for estimator in scores[1:]:
+            df = (df + pd.DataFrame(estimator,
+                                    index=feature_names,
+                                    columns=['Importance']))
+
+    df = df/scores.ndim
+    df = df.sort_values(by='Importance', ascending=False)
+    df.reset_index(inplace=True)
+    df.rename(columns={'index': 'Feature'}, inplace=True)
+
+    # ToDo: generic save_path param
+    if save_path is None:
+        df.to_csv('data/boston/feature_importance.csv', index=False)
+    return df
+
+@single_plot
+def plot_feat_imp(df, x, y, n_max=5):
+    df = df.head(n=n_max)
+    g = sns.barplot(x=x, y=y, data=df)
+    g.set_xlabel('Importance')
+    g.set_ylabel('Feature')
+    g.set_title('Feature importance for Boston dataset')
+    g.figure.savefig('figures/boston/{}.png'.format(y), bbox_inches="tight")
+    plt.show()
+
+if __name__ == "__main__":
+    from sklearn.datasets import load_boston
+    from sklearn.tree import DecisionTreeRegressor
+    from sklearn.model_selection import train_test_split, cross_validate
+
+    boston = load_boston()
+    reg = DecisionTreeRegressor()
+    scores = cross_validate(reg, boston['data'], boston['target'], cv=5,
+        scoring=['r2', 'neg_mean_squared_error'], return_estimator=True)
+    
+    list_fi = [fi.feature_importances_ for fi in scores['estimator']]
+    df = format_feat_imp(list_fi, boston['feature_names'])
+    plot_feat_imp(df, 'Feature', 'Importance', n_max=10)
+
+    
---
+++ src/my_plots.py
@@ -0,0 +1,49 @@
+import os
+import functools
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+'''
+    /usr/local/lib/python3.6/dist-packages/matplotlib/rcsetup.py
+    from docummentation:
+        fontsizes = ['xx-small', 'x-small', 'small', 'medium', 'large',
+                    'x-large', 'xx-large', 'smaller', 'larger']
+        
+        ls_mapper = {'-': 'solid', '--': 'dashed', '-.': 'dashdot', ':': 'dotted'}
+'''
+GLOBAL_CFG = {
+    'axes.labelsize': 'medium', # x and y labels
+    'xtick.labelsize': 'small', # x and y values
+    'ytick.labelsize': 'small',
+    'grid.linestyle': '--',
+    'figure.figsize': (12, 8),
+    'savefig.dpi': 100,
+    'axes.labelweight': 'bold'
+}
+
+def single_plot(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        n_max = kwargs['n_max'] if 'n_max' in kwargs else 5
+        palette = sns.color_palette('Blues_d', n_colors=n_max)
+        sns.set(style='darkgrid', palette=palette, color_codes=False, rc=GLOBAL_CFG)
+        func(*args, **kwargs)
+        # name = func.__name__
+        # arg_lst = []
+        # if args:
+        #     arg_lst.append(', '.join(repr(arg) for arg in args))
+        # if kwargs:
+        #     pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+        #     arg_lst.append(', '.join(pairs))
+        # arg_str = ', '.join(arg_lst)
+        # print('%s(%s)' % (name, arg_str,))
+    return inner
+
+@single_plot
+def plot_test():
+    g = sns.lineplot(x=[1,2,3], y=[4,5,6])
+    g.set_title('Title')
+    g.set_xlabel('X_label')
+    g.set_ylabel('Y_label')
+    plt.show()
